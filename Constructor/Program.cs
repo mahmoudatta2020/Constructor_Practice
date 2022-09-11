0a755b30619f9f9088f7fc3231de1470ec0bad16
unknown <redaattamahmoud@gmail.com>
2022-09-11 17:19:10 +0200

    myCons

---
+++ Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Constructor
+{
+    class car
+    {
+        public string model;
+        public string color;
+        public int year;
+        public car(string modelName,string colorName,int year2)
+        {
+            model = modelName;
+            color = colorName;
+            year = year2;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car BMW = new car("fnjnd","blue",2019);
+            
+            Console.WriteLine("Model is"+" "+BMW.model);
+            Console.WriteLine("Color is" + " " + BMW.color);
+            Console.WriteLine("Year is " + " " + BMW.year);
+        }
+    }
+}
